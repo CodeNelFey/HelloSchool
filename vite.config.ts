7a303de9f4e60ca4f449a1ed28f5fb01b39a3f4f
Sohan BIROTHEAU <sbirotheau@ippon.fr>
2025-06-05 10:44:34 +0200

    maj vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3030,
+    host: true,
+    allowedHosts: ['helloschool.sohan-birotheau.fr'],
+  },
 })
